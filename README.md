bfc8337e62f8c45ed7ae8f1c7ae41fec71edeeff
zjt <jintao7080@gmail.com>
2018-01-12 19:31:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# h2s
+h2s是一个把socks5代理转换为http,https代理的小工具,同时支持socks5,http,https
+
+# 安装
+```golang
+go get -v github.com/u35s/h2s
+```
+
+# 启动
+h2s默认会读取当前文件夹下的config.json配置,也可以用-c指定配置文件
+
+```
+h2s -c example.config.json
+```
+
+h2s也可以通过命令行参数指定相关参数
+
+```
+h2s -s "socks5.com:8388" -P "0.0.0.0:8088" -b "0.0.0.0" -l 1080 
+```
+
+* -P 本地http,https代理地址 eg: 0.0.0.0:8088
+* -s socks5服务器地址,当此地址包含端口时会忽略-p参数
+* -p socks5服务器端口
+* -b 本地socks5代理地址 eg: 127.0.0.1
+* -l 本地socks5代理地址端口 eg: 1080
+* -m 加密方法 default: aes-256-cfb
+* -d 开启调试日志
---
+++ http_proxy.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+
+	ss "github.com/shadowsocks/shadowsocks-go/shadowsocks"
+)
+
+func httpProxy(addr string) {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	debug.Printf("starting local http proxy server at %v ...\n", addr)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			debug.Printf("listen accept error %v\n", err)
+			break
+		}
+		go handleHttpProxyConn(conn)
+	}
+}
+
+func readHttpHead(conn net.Conn, stop int) *bytes.Buffer {
+	cur := &bytes.Buffer{}
+	bts := make([]byte, 1)
+	var last byte
+	for {
+		_, err := conn.Read(bts)
+		if err != nil {
+			break
+		} else if bts[0] == '\r' {
+			continue
+		} else if bts[0] == '\n' {
+			if stop == 1 {
+				break
+			} else if last == '\n' && stop == 2 {
+				break
+			}
+		}
+		last = bts[0]
+		cur.Write(bts)
+	}
+	return cur
+}
+
+func getHostPortType(line []byte) (host, port, tp string, err error) {
+	if n := len(line); n > 0 && line[n-1] == '\r' {
+		line = line[:n-1]
+	}
+	slc := strings.Split(string(line), " ")
+	if len(slc) < 2 {
+		fmt.Errorf("first line err %v", string(line))
+		return
+	}
+	switch slc[0] {
+	case "CONNECT":
+		hp := strings.Split(slc[1], ":")
+		if len(hp) != 2 {
+			err = fmt.Errorf("connect extract host,port err %v", slc[1])
+			return
+		}
+		host, port, tp = hp[0], hp[1], "https"
+	default:
+		thp := strings.Split(slc[1], "/")
+		if len(thp) < 3 {
+			err = fmt.Errorf("%v host err %v", slc[0], slc[1])
+			return
+		}
+		hp := strings.Split(thp[2], ":")
+		if len(hp) == 1 {
+			host, port, tp = hp[0], "80", "http"
+		} else if len(hp) == 2 {
+			host, port, tp = hp[0], hp[1], "http"
+		} else {
+			err = fmt.Errorf("%v extract host,port err %v", slc[0], slc[1])
+			return
+		}
+	}
+	return
+}
+
+func handleHttpProxyConn(conn net.Conn) {
+	defer conn.Close()
+	first := readHttpHead(conn, 1)
+	later := readHttpHead(conn, 2)
+
+	hosts, ports, tps, err := getHostPortType(first.Bytes())
+	if err != nil {
+		debug.Printf("get host,port,type error %v", err)
+		return
+	}
+
+	addr := bytes.Buffer{}
+	addr.WriteByte(0x03)
+	addr.WriteByte(byte(uint8(len(hosts))))
+	addr.WriteString(hosts)
+	port, err := strconv.ParseInt(ports, 10, 0)
+	if err != nil {
+		debug.Printf("https host port conv err,%v\n", ports)
+		return
+	}
+	//大端序
+	port16 := uint16(port)
+	addr.WriteByte(byte(uint8(port16 >> 8)))
+	addr.WriteByte(byte(uint8(port16 & 0x00ff)))
+	debug.Printf("connecting to %v:%v over %v\n", hosts, ports, tps)
+	debug.Printf("\n%v\n%v", first.String(), later.String())
+
+	remote, err := createServerConn(addr.Bytes(), hosts+":"+ports)
+	if err != nil {
+		debug.Printf("%v\n", err)
+		if len(servers.srvCipher) > 1 {
+			debug.Println("Failed connect to all avaiable shadowsocks server")
+		}
+		return
+	}
+	defer remote.Close()
+	if tps == "https" {
+		conn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+	} else {
+		remote.Write(first.Bytes())
+		remote.Write([]byte{'\n'})
+		remote.Write(later.Bytes())
+		remote.Write([]byte{'\n'})
+	}
+	go ss.PipeThenClose(conn, remote)
+	ss.PipeThenClose(remote, conn)
+	debug.Printf("closed connection to %v:%v", hosts, ports)
+}
